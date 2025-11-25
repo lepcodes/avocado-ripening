6a87141921d933fa27fcb08367b1b8b06b15624c
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2025-11-25 11:44:58 -0600

    Add Dataset Uploading to Kaggle dataset for training

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -1,12 +1,18 @@
+import json
+import os
+import shutil
+import zipfile
+
+import boto3
+import numpy as np
+import pandas as pd
+from botocore.client import Config
+from dotenv import load_dotenv
 from prefect import flow, task
 from prefect.logging import get_run_logger
-import pandas as pd
-import numpy as np
-import boto3
-from botocore.client import Config
-import zipfile
-from dotenv import load_dotenv
-import os
+
+load_dotenv()
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 load_dotenv()
 
@@ -116,17 +122,16 @@ def preprocess_data(raw_data_path: str):
         inplace=True,
     )
     data.dropna(inplace=True)
+    data["Absolute Path"] = data["File Name"].apply(lambda x: os.path.join(raw_data_path, "images", x + ".jpg"))
 
-    data["File Path"] = data["File Name"].apply(lambda x: raw_data_path + "/images/" + x + ".jpg")
-    data.drop(["File Name"], axis=1, inplace=True)
-
-    mask_existing_images = data["File Path"].apply(os.path.exists)
+    mask_existing_images = data["Absolute Path"].apply(os.path.exists)
+    logger.info(f"Unexisting images: {data['Absolute Path'].count() - mask_existing_images.sum()}")
     data = data[mask_existing_images].copy()
-
+    data.drop(["Absolute Path"], axis=1, inplace=True)
     # Final column reordering
     logger.info("Final column reordering...")
     target_columns = [
-        "File Path",       # 1st
+        "File Name",       # 1st
         "T10",             # 2nd
         "T20",             # 3rd
         "Tam",             # 4th
@@ -139,6 +144,90 @@ def preprocess_data(raw_data_path: str):
     # Preprocessing complete
     logger.info("Preprocessing complete.")
     return processed_dir
+
+@task
+def upload_preprocessed_data(processed_dir, raw_dir):
+    logger = get_run_logger()
+
+    logger.info("Creating staging area for dataset upload...")
+    staging_dir = os.path.join(os.path.dirname(processed_dir), "staging")
+    if os.path.exists(staging_dir):
+        shutil.rmtree(staging_dir)
+    os.makedirs(staging_dir, exist_ok=True)
+    os.makedirs(os.path.join(staging_dir, "images"), exist_ok=True)
+
+    logger.info("Copying dataset images to staging area...")
+    data = pd.read_csv(os.path.join(processed_dir, "data.csv"))
+    origin_images_dir = os.path.join(raw_dir, "images")
+    stagin_images_dir = os.path.join(staging_dir, "images")
+    count = 0
+    for file_name in data["File Name"]:
+        src_file = os.path.join(origin_images_dir, file_name + ".jpg")
+        dst_file = os.path.join(stagin_images_dir, file_name + ".jpg")
+        if os.path.exists(src_file):
+            shutil.copy2(src_file, dst_file)
+            count += 1
+        else:
+            logger.warning(f"❌ File listed in dataset but not found: {src_file}")
+    logger.info(f"✅ Copied {count} images to staging area.")
+
+    logger.info("Copying dataset csv to staging area...")
+    origin_csv_path = os.path.join(processed_dir, "data.csv")
+    staging_csv_path = os.path.join(staging_dir, "data.csv")
+    shutil.copy(origin_csv_path, staging_csv_path)
+
+    logger.info("Creating dataset metadata file...")
+    user = "luispreciado99"
+    dataset_slug = "avocado-ripening-dataset"
+    dataset_id = f"{user}/{dataset_slug}"
+    metadata = {
+        "title": "Avocado Ripening Dataset",
+        "id": dataset_id,
+        "licenses": [{"name": "CC0-1.0"}]
+    }
+    with open(os.path.join(staging_dir, "dataset-metadata.json"), "w") as f:
+        json.dump(metadata, f, indent=4)
+
+    logger.info("Authenticating with Kaggle API...")
+    try:
+        api = KaggleApi()
+        api.authenticate()
+    except Exception as e:
+        logger.error(f"⚠️ Error autenticando con Kaggle: {e}")
+        raise e
+
+    logger.info("Uploading dataset to Kaggle Datasets...")
+    message = "Uploading dataset structure (CSV + Images)"
+    try:
+        logger.info("Attempting to update dataset version...")
+        api.dataset_create_version(
+            staging_dir, 
+            version_notes=message, 
+            dir_mode='zip',
+            quiet=False
+        )
+        logger.info("✅ New dataset version created successfully.")
+        
+    except Exception as e:
+        error_str = str(e).lower()
+        if any(x in error_str for x in ["404", "not found", "403", "forbidden"]):
+            logger.info(f"⚠️ Dataset not updated (Error {error_str}).")
+            logger.info("🆕 Trying to create it from scratch...")
+            try:
+                api.dataset_create_new(
+                    staging_dir, 
+                    dir_mode='zip', 
+                    public=False,
+                    quiet=False
+                )
+                logger.info("✅ Dataset created successfully.")
+            except Exception as e2:
+                logger.error(f"❌ Fatal error trying to create: {e2}")
+                raise e2
+                
+        else:
+            logger.error(f"❌ Unexpected error not handled: {e}")
+            raise e
 
 
 @task
@@ -162,11 +251,15 @@ def deploy_model(data):
 @flow
 def test_flow():
     logger = get_run_logger()
-    data_path = fetch_data()
-    logger.info(f"Dataset path: {data_path}")
-    preprocessed_data_path = preprocess_data(data_path)
+
+    raw_data_path = fetch_data()
+    logger.info(f"Dataset path: {raw_data_path}")
+
+    preprocessed_data_path = preprocess_data(raw_data_path)
     logger.info(f"Preprocessed data path: {preprocessed_data_path}")
-    return data_path
+
+    upload_preprocessed_data(preprocessed_data_path, raw_data_path)
+    return raw_data_path
 
 
 if __name__ == "__main__":
