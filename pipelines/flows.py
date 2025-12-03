23e21ef75db680c1347616665cc639738d67d59e
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2025-12-02 19:31:27 -0600

    Fix logger hierarchy

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -18,8 +18,6 @@ from mlflow.tracking import MlflowClient
 load_dotenv()
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-load_dotenv()
-
 
 def compute_overripeness_time(time_stamp, overripe_time_stamp):
     if pd.isna(overripe_time_stamp):
@@ -43,16 +41,17 @@ def fetch_data(
     logger = get_run_logger()
     raw_dir = os.path.join(data_path, "raw")
     zip_path = os.path.join(raw_dir, file_key)
+    logger.info("📋 Beginning data acquisition...")
 
     # Caching (Check if the file already exists in the local path)
-    logger.info("Caching data... (Check raw data already exists)")
+    logger.info("\tCaching data... (Check raw data already exists)")
     os.makedirs(raw_dir, exist_ok=True)
     if os.path.exists(os.path.join(raw_dir, "data.csv")):
         logger.info("Raw data already exists. Skipping data acquisition.")
         return raw_dir
 
     # Fetch the data from the OCI bucket
-    logger.info(f"Fetching data from OCI bucket: {bucket_name}")
+    logger.info(f"\tFetching data from OCI bucket: {bucket_name}")
     local_path = os.path.join(data_path, "raw")
     zip_path = os.path.join(local_path, file_key)
     s3 = boto3.client(
@@ -65,14 +64,14 @@ def fetch_data(
     s3.download_file(bucket_name, file_key, zip_path)
 
     # Extract the data from the ZIP file
-    logger.info("Extracting data from ZIP file...")
+    logger.info("\tExtracting data from ZIP file...")
     with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(local_path)
 
     os.remove(zip_path)
 
     # Data acquisition complete
-    logger.info("Data acquisition complete.")
+    logger.info("\tData acquisition complete.")
     return raw_dir
 
 
@@ -82,23 +81,24 @@ def preprocess_data(raw_data_path: str):
     Preprocess Data
     """
     logger = get_run_logger()
+    logger.info("📋 Beginning data preprocessing...")
 
     # Go up one directory
     dataset_path = os.path.dirname(raw_data_path)
     processed_dir = os.path.join(dataset_path, "processed")
 
     # Caching (Check if the file already exists in the local path)
-    logger.info("Caching data... (Check processed data already exists)")
+    logger.info("\tCaching data... (Check processed data already exists)")
     if os.path.exists(os.path.join(processed_dir, "data.csv")):
         logger.info("Processed data already exists. Skipping preprocessing.")
         return processed_dir
 
     # Read the data from the CSV file
-    logger.info("Reading raw data...")
+    logger.info("\tReading raw data...")
     data = pd.read_csv(os.path.join(raw_data_path, "data.csv"))
 
     # Preprocess the data
-    logger.info("Preprocessing data...")
+    logger.info("\tPreprocessing data...")
     overripe_days = data[data["Ripening Index Classification"] == 5]
     first_overripe_day = overripe_days.groupby("Sample")[["Day of Experiment", "Time Stamp"]].min()
     data["Overripening Day"] = data["Sample"].map(first_overripe_day["Day of Experiment"])
@@ -129,11 +129,11 @@ def preprocess_data(raw_data_path: str):
     data["Absolute Path"] = data["File Name"].apply(lambda x: os.path.join(raw_data_path, "images", x + ".jpg"))
 
     mask_existing_images = data["Absolute Path"].apply(os.path.exists)
-    logger.info(f"Unexisting images: {data['Absolute Path'].count() - mask_existing_images.sum()}")
+    logger.info(f"\tUnexisting images: {data['Absolute Path'].count() - mask_existing_images.sum()}")
     data = data[mask_existing_images].copy()
     data.drop(["Absolute Path"], axis=1, inplace=True)
     # Final column reordering
-    logger.info("Final column reordering...")
+    logger.info("\tFinal column reordering...")
     target_columns = [
         "File Name",       # 1st
         "T10",             # 2nd
@@ -146,21 +146,22 @@ def preprocess_data(raw_data_path: str):
     data.to_csv(os.path.join(processed_dir, "data.csv"), index=False)
 
     # Preprocessing complete
-    logger.info("Preprocessing complete.")
+    logger.info("\tPreprocessing complete.")
     return processed_dir
 
 @task
 def upload_preprocessed_data(processed_dir, raw_dir):
     logger = get_run_logger()
+    logger.info("📋 Beginning dataset upload...")
 
-    logger.info("Creating staging area for dataset upload...")
+    logger.info("\tCreating staging area for dataset upload...")
     staging_dir = os.path.join(os.path.dirname(processed_dir), "staging")
     if os.path.exists(staging_dir):
         shutil.rmtree(staging_dir)
     os.makedirs(staging_dir, exist_ok=True)
     os.makedirs(os.path.join(staging_dir, "images"), exist_ok=True)
 
-    logger.info("Copying dataset images to staging area...")
+    logger.info("\tCopying dataset images to staging area...")
     data = pd.read_csv(os.path.join(processed_dir, "data.csv"))
     origin_images_dir = os.path.join(raw_dir, "images")
     stagin_images_dir = os.path.join(staging_dir, "images")
@@ -173,14 +174,14 @@ def upload_preprocessed_data(processed_dir, raw_dir):
             count += 1
         else:
             logger.warning(f"❌ File listed in dataset but not found: {src_file}")
-    logger.info(f"✅ Copied {count} images to staging area.")
+    logger.info(f"\tCopied {count} images to staging area.")
 
-    logger.info("Copying dataset csv to staging area...")
+    logger.info("\tCopying dataset csv to staging area...")
     origin_csv_path = os.path.join(processed_dir, "data.csv")
     staging_csv_path = os.path.join(staging_dir, "data.csv")
     shutil.copy(origin_csv_path, staging_csv_path)
 
-    logger.info("Creating dataset metadata file...")
+    logger.info("\tCreating dataset metadata file...")
     user = "luispreciado99"
     dataset_slug = "avocado-ripening-dataset"
     dataset_id = f"{user}/{dataset_slug}"
@@ -192,15 +193,15 @@ def upload_preprocessed_data(processed_dir, raw_dir):
     with open(os.path.join(staging_dir, "dataset-metadata.json"), "w") as f:
         json.dump(metadata, f, indent=4)
 
-    logger.info("Authenticating with Kaggle API...")
+    logger.info("\tAuthenticating with Kaggle API...")
     try:
         api = KaggleApi()
         api.authenticate()
     except Exception as e:
-        logger.error(f"⚠️ Error autenticando con Kaggle: {e}")
+        logger.error(f"⚠️ Error authenticating with Kaggle: {e}")
         raise e
 
-    logger.info("Uploading dataset to Kaggle Datasets...")
+    logger.info("\tUploading dataset to Kaggle Datasets...")
     message = "Uploading dataset structure (CSV + Images)"
     try:
         logger.info("Attempting to update dataset version...")
@@ -210,13 +211,13 @@ def upload_preprocessed_data(processed_dir, raw_dir):
             dir_mode='zip',
             quiet=False
         )
-        logger.info("✅ New dataset version created successfully.")
+        logger.info("\t✅ New dataset version created successfully.")
         
     except Exception as e:
         error_str = str(e).lower()
         if any(x in error_str for x in ["404", "not found", "403", "forbidden"]):
             logger.info(f"⚠️ Dataset not updated (Error {error_str}).")
-            logger.info("🆕 Trying to create it from scratch...")
+            logger.info("\t🆕 Trying to create it from scratch...")
             try:
                 api.dataset_create_new(
                     staging_dir, 
@@ -224,7 +225,7 @@ def upload_preprocessed_data(processed_dir, raw_dir):
                     public=False,
                     quiet=False
                 )
-                logger.info("✅ Dataset created successfully.")
+                logger.info("\t✅ Dataset created successfully.")
             except Exception as e2:
                 logger.error(f"❌ Fatal error trying to create: {e2}")
                 raise e2
@@ -242,16 +243,17 @@ def train_model(
     run_name: str,
 ):
     logger = get_run_logger()
+    logger.info("📋 Beginning model training...")
     KAGGLE_USERNAME = "luispreciado99"
     PROJECT_SLUG = "avocado-ripening-notebook"
     DATASET_SLUG = "avocado-ripening-dataset"
     NOTEBOOK_PATH = "notebooks/training-notebook-template.ipynb"
 
-    logger.info("Authenticating with Kaggle API...")
+    logger.info("\tAuthenticating with Kaggle API...")
     api = KaggleApi()
     api.authenticate()
 
-    logger.info("Creating metadata for kernel...")
+    logger.info("\tCreating metadata for kernel...")
     notebook_abs_path = os.path.abspath(NOTEBOOK_PATH)
     notebook_parent_dir = os.path.dirname(notebook_abs_path)
     notebook_staged_path = os.path.join(notebook_parent_dir, "training-notebook-staged.ipynb")
@@ -275,7 +277,7 @@ def train_model(
     with open(os.path.join(notebook_parent_dir, "kernel-metadata.json"), "w") as f:
         json.dump(metadata, f, indent=4)
 
-    logger.info("Injecting code cells...")
+    logger.info("\tInjecting code cells...")
     notebook = nbf.read(notebook_abs_path, as_version=4)
     if notebook.nbformat_minor < 5:
         notebook.nbformat_minor = 5
@@ -296,7 +298,7 @@ RUN_NAME = "{run_name}"
         notebook.nbformat_minor = 5
     nbf.write(notebook, notebook_staged_path)
 
-    logger.info("Pushing kernel...")
+    logger.info("\tPushing kernel...")
     try:
         api.kernels_push(notebook_parent_dir)
         logger.info("✅ Kernel pushed successfully.")
@@ -305,31 +307,31 @@ RUN_NAME = "{run_name}"
         logger.error(f"❌ Error pushing kernel: {e}")
         raise e
 
-    logger.info("Polling status loop...")
+    logger.info("\tPolling status loop...")
     while True:
         try:
             response = api.kernels_status(kernel_id)
             raw_status = str(response.status)
             status_clean = raw_status.split('.')[-1].lower()
-            logger.info(f"Status: {status_clean.upper()} (Raw: {raw_status}) | Time: {time.strftime('%H:%M:%S')}")
+            logger.info(f"\tStatus: {status_clean.upper()} (Raw: {raw_status}) | Time: {time.strftime('%H:%M:%S')}")
 
             if status_clean == 'complete':
-                logger.info("✅ Kernel finished successfully!")
+                logger.info("\t✅ Kernel finished successfully!")
                 break
             
             elif status_clean == 'error':
                 failure_msg = str(response.failureMessage)
-                raise Exception(f"Kaggle Training Failed: {failure_msg} | Time: {time.strftime('%H:%M:%S')}")
+                raise Exception(f"\t❌ Kaggle Training Failed: {failure_msg} | Time: {time.strftime('%H:%M:%S')}")
 
             elif status_clean in ['queued', 'running', 'starting', 'canceling']:
                 time.sleep(10)
             
             else:
-                logger.warning(f"⚠️ Unknown state {status_clean}. Keep waiting...")
+                logger.warning(f"\t⚠️ Unknown state {status_clean}. Keep waiting...")
                 time.sleep(20)
 
         except Exception as e:
-            logger.error(f"❌ Error checking status: {e}")
+            logger.error(f"\t❌ Error checking status: {e}")
             raise Failed(message=f"Error checking status: {e}")
     return run_id
 
@@ -341,6 +343,7 @@ def evaluate_model(
     experiment_name: str,
 ):
     logger = get_run_logger()
+    logger.info("📋 Beginning model evaluation...")
     client = MlflowClient("https://mlflow.lepcodes.com")
     
     # Search Experiment
@@ -356,27 +359,27 @@ def evaluate_model(
     challenger_run = runs[0]
     challenger_metric = challenger_run.data.metrics.get('accuracy_test', 0)
     challenger_run_id = challenger_run.info.run_id
-    logger.info(f"Challenger found: {challenger_metric}")
+    logger.info(f"\tChallenger found: {challenger_metric}")
 
     # Get Current Champion 
     try:
-        logger.info("Getting Champion...")
+        logger.info("\tGetting Champion...")
         champion_info = client.get_model_version_by_alias(model_name, "champion")
         champion_run = client.get_run(champion_info.run_id)
         champion_metric = champion_run.data.metrics.get('accuracy_test', 0)
-        logger.info(f"Champion found: {champion_metric}")
+        logger.info(f"\tChampion found: {champion_metric}")
     except Exception as e:
-        logger.info(f"No Champion found: {e}")
+        logger.info(f"\tNo Champion found: {e}")
         champion_metric = 0
 
     # Update Champion
     if challenger_metric > champion_metric:
         # Register Champion to Model Registry
-        logger.info("Promotion! Updating Champion...")
+        logger.info("\tPromotion! Updating Champion...")
         try:
             client.create_registered_model(model_name)
         except Exception as e:
-            logger.info(f"Champion already registered: {e}")
+            logger.info(f"\tChampion already registered: {e}")
 
         try:
             challenger_version = client.create_model_version(
@@ -385,11 +388,11 @@ def evaluate_model(
                 run_id=challenger_run_id
             )
             client.set_registered_model_alias(model_name, "champion", challenger_version.version)
-            logger.info(f"Champion registered: {challenger_version.version}")
+            logger.info(f"\tChampion registered: {challenger_version.version}")
         except Exception as e:
-            logger.info(f"Error registering Champion: {e}")
+            logger.info(f"\tError registering Champion: {e}")
     else:
-        logger.info("No Promotion. Keeping Champion...")
+        logger.info("\tNo Promotion. Keeping Champion...")
     return
 
 @task
